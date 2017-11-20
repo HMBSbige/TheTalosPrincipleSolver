1afa70ee861ff7b0c8002cb589b85f442d92811a
HMBSbige <admin@bige0.com>
2017-11-20 21:40:36 +0800

    add another way to create QR code

--- TheTalosPrincipleSolver/Properties/Resources.Designer.cs
+++ TheTalosPrincipleSolver/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace TheTalosPrincipleSolver.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap huaji512 {
+            get {
+                object obj = ResourceManager.GetObject("huaji512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap I {
             get {
                 object obj = ResourceManager.GetObject("I", resourceCulture);
--- TheTalosPrincipleSolver/QRCode.cs
+++ TheTalosPrincipleSolver/QRCode.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
+using TheTalosPrincipleSolver.Properties;
 using ZXing;
 using ZXing.Common;
 using ZXing.QrCode;
@@ -18,6 +20,13 @@ namespace TheTalosPrincipleSolver
         
         private void CreateQRcode(string str)
         {
+            if (str == string.Empty)
+            {
+                pictureBox1.Image = null;
+                CreateQRcode1(str);
+                return;
+            }
+            
             EncodingOptions options = new QrCodeEncodingOptions
             {
                 DisableECI = true,
@@ -34,24 +43,67 @@ namespace TheTalosPrincipleSolver
                 Options = options
             };
             
-            if (str == string.Empty)
-            {
-                pictureBox1.Image = null;
-                return;
-            }
             var bm = writer.Write(str);
 
-            //Bitmap overlay = new Bitmap(@"");
-
-            //int deltaHeigth = bm.Height - overlay.Height;
-            //int deltaWidth = bm.Width - overlay.Width;
-
-            //Graphics g = Graphics.FromImage(bm);
-            //g.DrawImage(overlay, new Point(deltaWidth / 2, deltaHeigth / 2));
+            using (var g = Graphics.FromImage(bm))
+            {
+                var custom = Resources.huaji512;
+                var w = (int)(Math.Min(options.Width, options.Height) * 0.2);
+                g.DrawImage(custom, new Rectangle((options.Width - w)/ 2, (options.Height-w)/2, w,w));
+            }
 
             pictureBox1.Image = bm;
         }
-
+        
+        private void CreateQRcode1(string str)
+        {
+            var width = pictureBox1.Width;
+            var height = pictureBox1.Height;
+            var size = Math.Min(pictureBox1.Width, pictureBox1.Height);
+            try
+            {
+                EncodingOptions options = new QrCodeEncodingOptions
+                {
+                    DisableECI = true,
+                    CharacterSet = @"UTF-8",
+                    Margin = 0
+                };// options.Hints
+                var code = Encoder.encode(str, ErrorCorrectionLevel.H, options.Hints);
+                var m = code.Matrix;
+                var blockSize = Math.Max(size / m.Width,1);
+                var bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                using (var g = Graphics.FromImage(bm))
+                {
+                    g.Clear(Color.White);
+                    using (Brush b = new SolidBrush(Color.Black))
+                    {
+                        for (var row = 0; row < m.Width; row++)
+                        {
+                            for (var col = 0; col < m.Height; col++)
+                            {
+                                if (m[row, col] != 0)
+                                {
+                                    g.FillRectangle(b,
+                                        (int)(blockSize * row + (width - size) / 2.0),
+                                        (int)(blockSize * col + (height - size) / 2.0),
+                                        blockSize, blockSize);
+                                }
+                            }
+                        }
+                        var custom = Resources.huaji512;
+                        var w = (int)(Math.Min(width, height) * 0.2);
+                        g.DrawImage(custom, new Rectangle((width - w) / 2, (height - w) / 2, w, w));
+                    }
+                }
+                
+                pictureBox1.Image = bm;
+            }
+            catch
+            {
+                //return;
+            }
+        }
+        
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             CreateQRcode(textBox1.Text);
